74dfd9a70b3f868f8d84e8bde851b861c56d0579
MAC <mac@MACs-MacBook-Pro.local>
2025-10-16 11:55:07 +0100

    Force full rebuild and refresh assets

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 /** Fix for `__dirname` in ES modules */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+// trigger redeploy
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
